d4360467f13e6c8061e40a6be182d1dd2c0ca5e4
hadekunle <shitoheeb@gmail.com>
2025-02-15 21:41:26 -0600

    add Lambda SNS terraform

--- 007_Lambda_SNS_Terraform/game_day_notifications.tf
+++ 007_Lambda_SNS_Terraform/game_day_notifications.tf
@@ -129,14 +129,16 @@ resource "aws_lambda_function" "nba_lambda" {
 # EventBridge Rule for Scheduling
 resource "aws_cloudwatch_event_rule" "nba_schedule" {
   name                = "nba_game_alerts_schedule"
-  schedule_expression = "rate(2 hours)" # Adjust as needed
+  schedule_expression = "rate(24 hours)"
+  force_destroy       = true
 }
 
 # EventBridge Target
 resource "aws_cloudwatch_event_target" "nba_target" {
-  rule      = aws_cloudwatch_event_rule.nba_schedule.name
-  target_id = "nba_lambda"
-  arn       = aws_lambda_function.nba_lambda.arn
+  rule          = aws_cloudwatch_event_rule.nba_schedule.name
+  target_id     = "nba_lambda"
+  arn           = aws_lambda_function.nba_lambda.arn
+  force_destroy = true
 }
 
 # Grant EventBridge Permission to Invoke Lambda
